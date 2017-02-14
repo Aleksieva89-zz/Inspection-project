c2649b837bfc34a2614e261b803e5ce2ea8b78a2
Valentina Aleksieva <martin@Martins-MacBook-Pro.local>
2017-02-14 22:15:27 +0100

    My first commit-Inspection project.

---
+++ src/com/company/GarageTest.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by martin on 2/14/17.
+ */
+public class GarageTest extends TestCase {
+
+    public void testNextCustomer() throws Exception {
+
+    }
+
+    public void testCloseDoor() throws Exception {
+
+    }
+}
